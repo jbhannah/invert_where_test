1b0f461d776eb658713f9aa913d6a1307518f1b3
Jesse B. Hannah <jesse@jbhannah.net>
2021-12-10 10:17:01 -0800

    Call #invert_where on just the scope to invert
    Using #and to chain a call to #invert_where on just the scope or query
    that you want to invert avoids the pitfall of inverting all preceding
    conditions in the query, simplifies the contents of .inverse_of, and
    generates properly inverted conditions instead of just wrapping them
    with a NOT clause.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
-  scope :inverse_of, ->(scope) { where(klass.send(scope).arel.constraints.reduce(:and).not) }
+  scope :inverse_of, ->(scope) { self.and(klass.send(scope).invert_where) }
 end

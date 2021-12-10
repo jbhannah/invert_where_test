9504ddde5a64174d45c6c31edfba548781a2ae0a
Jesse B. Hannah <jesse@jbhannah.net>
2021-12-10 09:25:09 -0800

    Pass scope name as a symbol and send to the model
    Directly passing the ActiveRecord::Relation to the scope results in
    adding redundant conditions to the inverse scope for all preceding query
    conditions. Sending the name of the scope to invert upstream, to the
    model class itself, retrieves and inverts only the conditions for that
    scope, avoiding any redundant conditions in the WHERE NOT clause.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
-  scope :inverse_of, ->(scope) { where(scope.arel.constraints.reduce(:and).not) }
+  scope :inverse_of, ->(scope) { where(klass.send(scope).arel.constraints.reduce(:and).not) }
 end
--- app/models/foobar.rb
+++ app/models/foobar.rb
@@ -2,11 +2,11 @@ class Foobar < ApplicationRecord
   after_initialize :set_expires_at
 
   scope :active, -> { where(expires_at: Time.zone.now..) }
-  scope :expired_inverse_of, -> { inverse_of(active) }
+  scope :expired_inverse_of, -> { inverse_of(:active) }
   scope :expired_invert_where, -> { active.invert_where }
 
   scope :published, -> { where(published: true) }
-  scope :draft_inverse_of, -> { inverse_of(published) }
+  scope :draft_inverse_of, -> { inverse_of(:published) }
   scope :draft_invert_where, -> { published.invert_where }
 
   private

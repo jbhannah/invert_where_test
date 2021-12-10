d4699476a3d0f9d9c5faba3d84b84415e067640a
Jesse B. Hannah <jesse@jbhannah.net>
2021-12-09 17:21:18 -0800

    Foobar.active scope

--- app/models/foobar.rb
+++ app/models/foobar.rb
@@ -1,6 +1,8 @@
 class Foobar < ApplicationRecord
   after_initialize :set_expires_at
 
+  scope :active, -> { where(expires_at: Time.zone.now..) }
+
   private
   def set_expires_at
     self.expires_at ||= 30.days.from_now
--- test/models/foobar_test.rb
+++ test/models/foobar_test.rb
@@ -1,9 +1,30 @@
 require "test_helper"
 
 class FoobarTest < ActiveSupport::TestCase
-  test "sets expired_at to 30 days from now" do
+  test "defaults #expired_at to 30 days from now" do
     travel_to "2021-12-10 00:04:00" do
       assert Foobar.new.expires_at == "2022-01-09 00:04:00"
+    end
+  end
+
+  test "is .active if the timestamp is in the future" do
+    foobar = Foobar.first
+    travel_to 1.minute.before(foobar.expires_at) do
+      assert_includes Foobar.active, foobar
+    end
+  end
+
+  test "is .active if the timestamp is now" do
+    foobar = Foobar.first
+    travel_to foobar.expires_at do
+      assert_includes Foobar.active, foobar
+    end
+  end
+
+  test "is not .active if the timestamp is in the past" do
+    foobar = Foobar.first
+    travel_to 1.minute.after(foobar.expires_at) do
+      assert (not Foobar.active.include?(foobar))
     end
   end
 end
